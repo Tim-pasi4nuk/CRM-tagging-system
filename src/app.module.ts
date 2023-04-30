85e5174946ed58db46850470d9b31614cc8dd8ba
TIMOFIY PASICHNUK <timkapva13@gmail.com>
2023-05-01 01:08:38 +0300

    init modules: order, tag, user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { OrderModule } from './order/order.module';
+import { TagModule } from './tag/tag.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, OrderModule, TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
